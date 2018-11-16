bb88a2c242987d50b58d6cdb6b6eb8a34a576405
Merlin Reichwald <merlin.reichwald@gmail.com>
2018-11-16 02:24:33 +0100

    instant numbers, small ranking symbols

---
+++ Source/Standard/Hitcircle/Instant Numbers/0-to-9.bat
@@ -0,0 +1,10 @@
+@echo off
+cp default-0.png default-1.png
+cp default-0.png default-2.png
+cp default-0.png default-3.png
+cp default-0.png default-4.png
+cp default-0.png default-5.png
+cp default-0.png default-6.png
+cp default-0.png default-7.png
+cp default-0.png default-8.png
+cp default-0.png default-9.png
